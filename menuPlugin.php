20d77e76f8ce6a232cb3fe7205e370d00b7b42c7
Christian <christian.xperius@gmail.com>
2016-05-17 12:31:49 -0400

    added form input hidden to manage data through another functions

--- menuPlugin.php
+++ menuPlugin.php
@@ -17,8 +17,21 @@ class Menu_plugin{
 
 	//callback function
 	public function admin_page(){
+		if($_POST['menu_hidden'] == 'Y'):
+			$this->handleForm($_POST);
+		else:
 		$html = "A WordPress plugin for showing a three vertical menu";
+		$html .= "<h2>" . __( 'Menu Display Options', 'menu_trdom' ) . "</h2>";
+		$html .= '<form name="menu_form" method="post" action="'. str_replace( '%7E', '~', $_SERVER['REQUEST_URI']) .'">';
+		$html .= '<input type="hidden" name="menu_hidden" value="Y">';
+		$html .= '<input type="submit" name="Submit" value="Update Options" />';
+		$html .= '</form>';
 		echo $html;
+		endif;
+	}
+
+	public function handleForm($data){
+		var_dump($data);
 	}
 
 }
